a3891fe753b7602532ff713517932ab6fa99da8f
Silencecalls <Silencecalls@9b29db34-ac76-11de-a87c-5360b77f26dc>
2009-11-05 19:30:16 +0000

    Extracted two classes.

---
+++ pacManKeyAdapter.java
@@ -0,0 +1,104 @@
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+// Key event handlers
+class pacManKeyAdapter extends KeyAdapter
+{
+   PacMan   m_pacMan;
+   
+   pacManKeyAdapter (PacMan pacMan)
+   {
+      super ();
+      m_pacMan = pacMan;
+   }
+   
+   public void keyPressed(KeyEvent event)
+   {
+      switch (event.getKeyCode()) 
+      {
+      case KeyEvent.VK_A:
+     	   m_pacMan.m_gameModel.m_ghostPlayer.m_requestedDirection = Thing.LEFT;
+           break;
+     
+     case KeyEvent.VK_D:
+			   m_pacMan.m_gameModel.m_ghostPlayer.m_requestedDirection = Thing.RIGHT;
+           break;
+     
+     case KeyEvent.VK_W:
+           m_pacMan.m_gameModel.m_ghostPlayer.m_requestedDirection = Thing.UP;
+			   break;
+     
+     case KeyEvent.VK_S:
+				m_pacMan.m_gameModel.m_ghostPlayer.m_requestedDirection = Thing.DOWN;
+           break;
+      case KeyEvent.VK_LEFT:
+      	   m_pacMan.m_gameModel.m_player.m_requestedDirection = Thing.LEFT;
+            break;
+      
+      case KeyEvent.VK_RIGHT:
+			   m_pacMan.m_gameModel.m_player.m_requestedDirection = Thing.RIGHT;
+            break;
+      
+      case KeyEvent.VK_UP:
+            m_pacMan.m_gameModel.m_player.m_requestedDirection = Thing.UP;
+			   break;
+      
+      case KeyEvent.VK_DOWN:
+				m_pacMan.m_gameModel.m_player.m_requestedDirection = Thing.DOWN;
+            break;
+      
+      case KeyEvent.VK_N:
+    	  	//make sure we DO NOT carry over multiplayer state accidently
+    	    //as such we set multiplayer as false
+    	  	m_pacMan.m_gameModel.multiplayer = false;
+            m_pacMan.m_gameModel.m_state = GameModel.STATE_NEWGAME;
+            m_pacMan.m_gameUI.m_bDrawPaused = false;
+            break;
+            
+      case KeyEvent.VK_M:
+    	  	//set multiplayer as true.
+    	  	m_pacMan.m_gameModel.multiplayer = true;
+    	  	m_pacMan.m_gameModel.m_state = GameModel.STATE_NEWGAME;
+    	  	m_pacMan.m_gameUI.m_bDrawPaused = false;
+    	  	break;
+            
+      
+      case KeyEvent.VK_P:
+            if (m_pacMan.m_gameModel.m_state == GameModel.STATE_GAMEOVER)
+               break;
+            
+            if (m_pacMan.m_gameModel.m_state == GameModel.STATE_PAUSED)
+            {
+               m_pacMan.m_gameModel.m_state = m_pacMan.m_gameModel.m_pausedState;
+               m_pacMan.m_gameUI.m_bDrawPaused = false;
+               m_pacMan.m_gameUI.m_bRedrawAll = true;
+               
+            } else {
+               m_pacMan.m_gameModel.m_pausedState = m_pacMan.m_gameModel.m_state;
+               m_pacMan.m_gameModel.m_state = GameModel.STATE_PAUSED;
+            }
+            break;
+            
+      //K is for ABOUT      
+      case KeyEvent.VK_K:
+            m_pacMan.m_gameModel.m_state = GameModel.STATE_ABOUT;
+            m_pacMan.m_gameModel.m_nTicks2AboutShow = 0;
+            break;
+            
+      //V is for SOUND
+      case KeyEvent.VK_V:
+            m_pacMan.m_soundMgr.m_bEnabled = !m_pacMan.m_soundMgr.m_bEnabled;
+            if (m_pacMan.m_soundMgr.m_bEnabled == false)
+               m_pacMan.m_soundMgr.stop ();
+            m_pacMan.m_bottomCanvas.repaint ();
+            break;
+             
+      case KeyEvent.VK_I:
+            m_pacMan.toggleGhostAI ();
+            break;
+      
+      default:
+				System.out.println("Hello World!");
+      }   
+   }
+}
